58bda1c30d781d7921c28c002e286255707b50c2
Perry Smith <pedz@easesoftware.com>
2014-01-07 19:15:32 -0600

    First attempt.
    Appears to be working from the laptop but that is not a 100% test.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 gsa-cscope
 ==========
 
-Add-on to cscope to help with GSA and OSF build trees
+Add-on to cscope to help with GSA and OSF build trees.
+
+This adds to the cscope-dir-patterns to match a users sandbox and the
+backing trees held in GSA.  It also creates convenience functions to
+start cscope within a particular backing tree.  Last, it adds in the
+osf-dce enhancement to find files held in the backing tree when given
+a file in a sandbox (i.e. it understands the OSF build system).
---
+++ gsa-cscope.el
@@ -0,0 +1,124 @@
+;;; gsa-cscope.el --- GSA additions to cscope
+
+;; We really need cscope to work :-)
+(require 'cscope)
+
+;; Always drag in the ofs-dce feature
+(require 'osf-dce)
+
+(defgroup gsa-cscope nil
+  "GSA cscope additions"
+  :group 'languages)
+
+(defcustom gsa-cscope-sandbox-path
+  (file-name-as-directory
+   (if (file-exists-p (expand-file-name "~/.sandboxrc"))
+       (let* ((sandbox (find-file (expand-file-name "~/.sandboxrc")))
+	      (path (if (re-search-forward "^base \\* \\(.*\\)" nil t)
+			(match-string 1))))
+	 (kill-buffer sandbox)
+	 path)
+     (expand-file-name "~/sandbox")))
+  "Path to the base of the user's sandboxes.
+The default is dug out of the user's .sandboxrc file if possible.
+Otherwise, ~/sandbox is used"
+  :group 'gsa-cscope
+  :type 'string)
+
+(defcustom gsa-cscope-dir-patterns
+  (list
+   ;; Pattern to match files found in backing trees
+   '("^\\(/gsa/.*/aix\\(53\\|61\\|71\\)./\\)[^/]*/"
+     (concat (match-string 1 default-directory) "cscope/bin/cscope") ; which cscope to use
+     "-d -q -l"							     ; options to pass
+     (match-string 0 default-directory)				     ; to level dir
+     (concat (match-string 1 default-directory) "cscope/mono.db"))   ; cscope database
+
+   ;; Pattern to match files found in a sandbox
+   '((concat gsa-cscope-sandbox-path "[^/]+/")
+     (concat
+      (file-name-as-directory			;add /
+       (directory-file-name			;move up to parent
+	(file-name-directory			;remove any /
+	 (file-truename				;walk sym links
+	  (concat				;append "link" to get to backing tree
+	   (file-name-as-directory		;add / if needed
+	    (match-string 0 default-directory)) ;path to sandbox
+	   "link")))))
+      "cscope/bin/cscope")			;which cscope to use
+     "-d -q -l"					;options to pass to cscope
+     (match-string 0 default-directory)
+     (concat					;append cscope/mono.db
+      (file-name-as-directory			;add /
+       (directory-file-name			;move up to parent
+	(file-name-directory			;remove any /
+	 (file-truename				;walk sym links
+	  (concat				;append "link"
+	   (file-name-as-directory		;add / if needed
+	    (match-string 0 default-directory)) ;path to sandbox
+	   "link")))))
+      "cscope/mono.db")))
+  "A list of patterns to prepend to `cscope-dir-patterns'.
+The default adds two patterns.  One matches files in the backing trees.
+The second matches files in the user's sandbox."
+  :type 'list
+  :group 'gsa-cscope)
+
+(defcustom gsa-cscope-match-cache
+  (expand-file-name "~/.emacs.d/gsa_tree_cache.el")
+  "Path to an elisp file that is created and maintained by the
+`gsa-create-cscope-matches' command"
+  :type 'string
+  :group 'gsa-cscope)
+
+;; Prepend the GSA patters since they should be more specific.
+(setq cscope-dir-patterns (append gsa-cscope-dir-patterns cscope-dir-patterns))
+
+(defun gsa-cscope-matches nil
+  "The list of directories that match the patterns in `gsa-pattern-list'.
+`gsa-create-cscope-matches' is used to create and update
+`gsa-cscope-match-cache' which is an elisp file that sets the value to
+  `gsa-cscope-matches'.")
+
+(defun gsa-create-cscope-matches ()
+  "Creates the file at `gsa-cscope-match-cache' which defines `gsa-cscope-matches'.
+It then loads the file after creating it."
+  (interactive)
+  (let* ((file (find-file gsa-cscope-match-cache))
+	 (the-list (apply 'append (mapcar 'file-expand-wildcards gsa-pattern-list))))
+    (erase-buffer)
+    (insert (format "(setq gsa-cscope-matches '%S)" the-list))
+    (basic-save-buffer)
+    (kill-buffer file))
+  (gsa-cscope-load-match-cache))
+
+(defvar gsa-cscope-obarray nil
+  "An obarray of the base component for each of the paths in `gsa-cscope-matches'.")
+
+(defun gsa-cscope-load-match-cache ()
+  "Loads the file specified by `gsa-cscope-match-cache'.
+The load should set `gsa-cscope-matches' which is then used to create
+`gsa-cscope-obarray'."
+  (interactive)
+  (load gsa-cscope-match-cache)
+  (setq gsa-cscope-obarray (make-vector 23 0))
+  (mapc (lambda (dir)
+	  (put (intern (file-name-nondirectory (directory-file-name dir)) gsa-cscope-obarray)
+	       'full-path dir))
+	gsa-cscope-matches))
+
+(if (file-exists-p gsa-cscope-match-cache)
+    (gsa-cscope-load-match-cache))
+
+(defun gsa-cscope-start ( sym )
+  (interactive (list (completing-read "Prompt: " gsa-cscope-obarray)))
+  (unless (symbolp sym)
+    (setq sym (intern-soft sym gsa-cscope-obarray)))
+  (let* ((dir (file-name-as-directory (get sym 'full-path)))
+	 (parent (file-name-as-directory
+		  (directory-file-name
+		   (file-name-directory dir))))
+	 (cscope (concat parent "cscope/bin/cscope"))
+	 (options "-d -q -l")
+	 (database (concat parent "cscope/mono.db")))
+    (cscope-init-process cscope options dir database)))
